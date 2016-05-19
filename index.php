8d9bede872efffb67d26ac0b5b6ca98b1026c6bf
royalmarin520 <dennis.caroli194@outlook.com>
2016-05-19 13:10:05 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,5 @@
+<!DOCUMENT TYPE HTML>
+<html> <body>
+<h1>Pagina di prova, lavori in corso</h1>
+<a href="https://github.com/royalmarin520/tesina">Qui potrai trovare la repository su github</a>
+</body></html>
