7dfda7895d1838e8aad80e1e556de8d1cdabca1e
royalmarin520 <dennis.caroli194@outlook.com>
2016-05-20 13:23:10 +0200

    quarto commit

--- index.php
+++ index.php
@@ -2,5 +2,5 @@
 <html> <body>
 <h1>Pagina di prova, lavori in corso</h1>
 <a href="https://github.com/royalmarin520/tesina">Qui potrai trovare la repository su github</a>
-Questa è la modifica effettuata al coso del coso
+Quarta modifica
 </body></html>
