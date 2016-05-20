065981ecf786ef5924f606e64a24a778164f9b12
royalmarin520 <dennis.caroli194@outlook.com>
2016-05-20 12:59:14 +0200

    Secondo commit, mi ero dimenticato di aggiungere la chiave ssh all'account openshift

--- index.php
+++ index.php
@@ -2,5 +2,5 @@
 <html> <body>
 <h1>Pagina di prova, lavori in corso</h1>
 <a href="https://github.com/royalmarin520/tesina">Qui potrai trovare la repository su github</a>
-CIA1
+Secondo commit di prova
 </body></html>
