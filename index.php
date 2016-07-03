cac4e315da241ad554cfa7ae453b0d26cb140a15
royalmarin520 <dennis.caroli194@outlook.com>
2016-07-03 21:04:11 +0200

    Modifica alla dimostrazione

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <!DOCUMENT TYPE HTML>
 <html> <body>
-<h1>Pagina di prova, lavori in corso</h1>
+<h1>Pagina di prova, lavori in corsoprova</h1>
 <a href="https://github.com/royalmarin520/tesina">Qui potrai trovare la repository su github</a>
 Quarta modifica
 </body></html>
